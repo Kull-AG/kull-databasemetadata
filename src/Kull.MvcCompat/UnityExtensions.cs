c709a635c3007e2ee19ee0b8c533895462154f84
Adrian Ehrsam <ehrsam@kull.ch>
2020-05-20 11:38:04 +0200

    some more extension methods

--- src/Kull.MvcCompat/UnityExtensions.cs
+++ src/Kull.MvcCompat/UnityExtensions.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
 using Unity;
+using Unity.Injection;
 using Unity.Lifetime;
 
 namespace Kull.MvcCompat
@@ -17,11 +18,61 @@ namespace Kull.MvcCompat
             return unityContainer;
         }
 
+        public static IUnityContainer AddTransient<T, T2>(this IUnityContainer unityContainer)
+        {
+            unityContainer.RegisterType(typeof(T), typeof(T2), new TransientLifetimeManager());
+            return unityContainer;
+        }
+
+        public static IUnityContainer AddTransient<T>(this IUnityContainer unityContainer,
+            Func<IUnityContainer, T> func)
+        {
+            unityContainer.RegisterFactory<T>(c => func(c), new TransientLifetimeManager());
+            return unityContainer;
+        }
+
+        public static IUnityContainer AddSingleton<T>(this IUnityContainer unityContainer,
+            Func<IUnityContainer, T> func)
+        {
+            unityContainer.RegisterFactory<T>(c => func(c), new SingletonLifetimeManager());
+            return unityContainer;
+        }
+
+        public static IUnityContainer AddScoped<T>(this IUnityContainer unityContainer,
+           Func<IUnityContainer, T> func)
+        {
+            unityContainer.RegisterFactory<T>(c => func(c), new PerThreadLifetimeManager());
+            return unityContainer;
+        }
+
+
         public static IUnityContainer AddSingleton<T>(this IUnityContainer unityContainer)
         {
             unityContainer.RegisterType(typeof(T), new SingletonLifetimeManager());
             return unityContainer;
         }
+
+        public static IUnityContainer TryAddSingleton<T>(this IUnityContainer unityContainer)
+        {
+            if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
+            unityContainer.RegisterType(typeof(T), new SingletonLifetimeManager());
+            return unityContainer;
+        }
+
+        public static IUnityContainer TryAddTransient<T>(this IUnityContainer unityContainer)
+        {
+            if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
+            unityContainer.RegisterType(typeof(T), new TransientLifetimeManager());
+            return unityContainer;
+        }
+
+        public static IUnityContainer TryAddScoped<T>(this IUnityContainer unityContainer)
+        {
+            if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
+            unityContainer.RegisterType(typeof(T), new PerThreadLifetimeManager());
+            return unityContainer;
+        }
+
         public static IUnityContainer AddScoped<T>(this IUnityContainer unityContainer)
         {
             // Seems to be the closest match
