c6e8c6f37ad043e6ae1de242446f94a150d72d4b
Adrian Ehrsam <ehrsam@kull.ch>
2020-12-08 08:18:32 +0100

    fixes missing dispose

--- src/Kull.MvcCompat/UnityExtensions.cs
+++ src/Kull.MvcCompat/UnityExtensions.cs
@@ -17,22 +17,25 @@ namespace Kull.MvcCompat
     /// </summary>
     public static partial class UnityExtensions
     {
+        public static Func<ITypeLifetimeManager> createTransient = ()=> new PerRequestLifetimeManager();
+        public static Func<ITypeLifetimeManager> createScoped = () => new PerRequestLifetimeManager();
+
         public static IUnityContainer AddTransient<T>(this IUnityContainer unityContainer)
         {
-            unityContainer.RegisterType(typeof(T), GetRandomId<T>(unityContainer), new TransientLifetimeManager());
+            unityContainer.RegisterType(typeof(T), GetRandomId<T>(unityContainer), createTransient());
             return unityContainer;
         }
 
         public static IUnityContainer AddTransient<T, T2>(this IUnityContainer unityContainer)
         {
-            unityContainer.RegisterType(typeof(T), typeof(T2), GetRandomId<T>(unityContainer), new TransientLifetimeManager());
+            unityContainer.RegisterType(typeof(T), typeof(T2), GetRandomId<T>(unityContainer), createTransient());
             return unityContainer;
         }
 
         public static IUnityContainer AddTransient<T>(this IUnityContainer unityContainer,
             Func<IServiceProvider, T> func)
         {
-            unityContainer.RegisterFactory<T>(GetRandomId<T>(unityContainer), c => func(GetIServiceProvider(c)), new TransientLifetimeManager());
+            unityContainer.RegisterFactory<T>(GetRandomId<T>(unityContainer), c => func(GetIServiceProvider(c)),(IFactoryLifetimeManager) createTransient());
             return unityContainer;
         }
         private static IServiceProvider GetIServiceProvider(IUnityContainer c)
@@ -69,7 +72,7 @@ namespace Kull.MvcCompat
         {
             unityContainer.RegisterFactory<T>(
                  GetRandomId<T>(unityContainer),
-                 c => func(GetIServiceProvider(c)), new PerRequestLifetimeManager());
+                 c => func(GetIServiceProvider(c)), (IFactoryLifetimeManager)createScoped());
             return unityContainer;
         }
 
@@ -89,7 +92,7 @@ namespace Kull.MvcCompat
         public static IUnityContainer AddScoped<T>(this IUnityContainer unityContainer)
         {
             // Seems to be the closest match
-            unityContainer.RegisterType(typeof(T), GetRandomId<T>(unityContainer), new PerRequestLifetimeManager());
+            unityContainer.RegisterType(typeof(T), GetRandomId<T>(unityContainer), createScoped());
             return unityContainer;
         }
         public static IUnityContainer TryAddSingleton<T>(this IUnityContainer unityContainer)
@@ -109,21 +112,21 @@ namespace Kull.MvcCompat
         public static IUnityContainer TryAddTransient<T>(this IUnityContainer unityContainer)
         {
             if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
-            unityContainer.RegisterType(typeof(T), new TransientLifetimeManager());
+            unityContainer.RegisterType(typeof(T), createTransient());
             return unityContainer;
         }
 
         public static IUnityContainer TryAddTransient<T, T2>(this IUnityContainer unityContainer)
         {
             if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
-            unityContainer.RegisterType(typeof(T), typeof(T2), new TransientLifetimeManager());
+            unityContainer.RegisterType(typeof(T), typeof(T2), createTransient());
             return unityContainer;
         }
 
         public static IUnityContainer TryAddScoped<T>(this IUnityContainer unityContainer)
         {
             if (unityContainer.IsRegistered(typeof(T))) return unityContainer;
-            unityContainer.RegisterType(typeof(T), new PerRequestLifetimeManager());
+            unityContainer.RegisterType(typeof(T), createScoped());
             return unityContainer;
         }
 
