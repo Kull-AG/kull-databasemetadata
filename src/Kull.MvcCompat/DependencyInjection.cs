ca90b8e03fdd2ec9118eda8519a8d9a2427d5ff1
Adrian Ehrsam <ehrsam@kull.ch>
2020-12-18 20:00:50 +0100

    add http context accessor

--- src/Kull.MvcCompat/DependencyInjection.cs
+++ src/Kull.MvcCompat/DependencyInjection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity;
+using Unity.AspNet.Mvc;
 using Unity.Lifetime;
 
 namespace Kull.MvcCompat
@@ -15,6 +16,11 @@ namespace Kull.MvcCompat
             services.RegisterType(typeof(ILogger<>), typeof(Logger<>), new SingletonLifetimeManager());
             services.RegisterType(typeof(IHostingEnvironment), typeof(HostingEnvironment), new SingletonLifetimeManager());
             services.RegisterFactory(typeof(IServiceProvider), (c)=> new ServiceProviderUnity(c), new TransientLifetimeManager());
+        }
+
+        public static void AddHttpContextAccessor(this IUnityContainer services)
+        {
+            services.RegisterType(typeof(IHttpContextAccessor), typeof(HttpContextAccessor), new PerRequestLifetimeManager());
         }
     }
 }
---
+++ src/Kull.MvcCompat/IHttpContextAccessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Kull.MvcCompat
+{
+    // see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-context?view=aspnetcore-5.0
+    public interface IHttpContextAccessor
+    {
+        HttpContextBase ? HttpContext { get; }
+    }
+
+    public class HttpContextAccessor : IHttpContextAccessor
+    {
+        private HttpContextBase? _context;
+        public HttpContextBase? HttpContext
+        {
+            get
+            {
+                _context = _context ?? (System.Web.HttpContext.Current == null ? null : new System.Web.HttpContextWrapper(System.Web.HttpContext.Current));
+                return _context;
+            }
+        }
+    }
+}
